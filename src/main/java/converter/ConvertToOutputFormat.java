ae4d010a79bd0b672a118079890f7c61ce2be07c
Semen Nikolaenko <nikolaenko.semen12@gmail.com>
2021-03-15 22:27:39 +0300

    trying to create javadoc for converter

--- src/main/java/converter/ConvertToOutputFormat.java
+++ src/main/java/converter/ConvertToOutputFormat.java
@@ -6,22 +6,35 @@ import output.OutputObject;
 
 import java.util.*;
 
+/**
+ * @version 1
+ */
 @Component
 public class ConvertToOutputFormat implements Converter {
 
-@Override
+
+    @Override
     public OutputObject getReadyObject(String lineReadyToConvert) {
         OutputObject readyObject;
 
-            if (lineReadyToConvert.contains(Errors.READ_ERROR.getMessage())) {
-                readyObject = new OutputObject.Builder().setResult(Errors.READ_ERROR.getMessage()).build();
+        /*
+        если строка после парсинга содержит сообщение об ошибке, предарительно помещенное
+        в нее парсером, то готовый объект заполняется этим сообщением, для дальнешего вывода статуса в консоль
+         */
+        if (lineReadyToConvert.contains(Errors.READ_ERROR.getMessage())) {
+            readyObject = new OutputObject.Builder().setResult(Errors.READ_ERROR.getMessage()).build();
+                /*
+                проверка аналогичная предыдущей, но на наличие другой ошибки
+                 */
+        } else if (lineReadyToConvert.contains(Errors.DONT_ENOUGH_INPUT_DATA.getMessage())) {
+            readyObject = new OutputObject.Builder().setResult(Errors.DONT_ENOUGH_INPUT_DATA.getMessage()).build();
+            //если ошибок в строке не найдено, происходит конвертация в заданный формат
+        } else {
+            try {
+                //из переопределенного метода getParamsForFutureObject вытаскиваются параметры
+                Map<String, String> params = getParamsForFutureObject(lineReadyToConvert);
 
-            } else if (lineReadyToConvert.contains(Errors.DONT_ENOUGH_INPUT_DATA.getMessage())) {
-                 readyObject = new OutputObject.Builder().setResult(Errors.DONT_ENOUGH_INPUT_DATA.getMessage()).build();
-
-            } else {
-                try {
-                    Map<String, String> params = getParamsForFutureObject(lineReadyToConvert);
+                //происходит создание требуемого объекта с помощью параметров полученных из мапы
 
                 readyObject = new OutputObject.Builder()
                         .setId(Integer.parseInt(params.get("orderId")))
@@ -31,27 +44,32 @@ public class ConvertToOutputFormat implements Converter {
                         .setResult(params.get("result"))
                         .setComment(params.get("comment"))
                         .build();
-                }catch (NumberFormatException e){
-                    return readyObject = new OutputObject.Builder().setResult(Errors.INVALID_DATA_INPUT.getMessage()).build();
-                }
+            } catch (NumberFormatException e) {
+              /*
+                если в первоначальной строке были введены неверные параметры
+                например вместо числа была введена строка, парсер такую ошибку не заметил,
+                но на этапе конвертации такая ошибка фиксируется и обрабатывается
+               */
+                return readyObject = new OutputObject.Builder().setResult(Errors.INVALID_DATA_INPUT.getMessage()).build();
             }
+        }
         return readyObject;
     }
 
     @Override
     public Map<String, String> getParamsForFutureObject(String convertedLine) throws NumberFormatException {
         Map<String, String> paramsMap = new HashMap<>();
-            paramsMap.put("orderId", String.valueOf(getOrderIdtFromParsingString(convertedLine)));
-            paramsMap.put("amount", String.valueOf(getAmountFromParsingString(convertedLine)));
-            paramsMap.put("comment", getCommentFromParsingString(convertedLine));
-            paramsMap.put("filename", getFileNameFromParsingString(convertedLine));
-            paramsMap.put("line", String.valueOf(getLineNumberFromParsingString(convertedLine)));
-            paramsMap.put("result", "OK");
+        paramsMap.put("orderId", String.valueOf(getOrderIdtFromParsingString(convertedLine)));
+        paramsMap.put("amount", String.valueOf(getAmountFromParsingString(convertedLine)));
+        paramsMap.put("comment", getCommentFromParsingString(convertedLine));
+        paramsMap.put("filename", getFileNameFromParsingString(convertedLine));
+        paramsMap.put("line", String.valueOf(getLineNumberFromParsingString(convertedLine)));
+        paramsMap.put("result", "OK");
         return paramsMap;
     }
 
-    @Override
-    public String getCommentFromParsingString(String convertedLine) {
+
+    private String getCommentFromParsingString(String convertedLine) {
         String comment = convertedLine
                 .substring(convertedLine.indexOf("comment "), convertedLine.indexOf("filename"))
                 .substring(8)
@@ -59,29 +77,29 @@ public class ConvertToOutputFormat implements Converter {
         return comment;
     }
 
-    @Override
-    public int getOrderIdtFromParsingString(String convertedLine) throws NumberFormatException{
+
+    private int getOrderIdtFromParsingString(String convertedLine) throws NumberFormatException {
         String orderIdString = convertedLine
                 .substring(convertedLine.indexOf("orderId "), convertedLine.indexOf("amount"))
                 .substring(8)
                 .trim();
-            int orderId = Integer.parseInt(orderIdString);
+        int orderId = Integer.parseInt(orderIdString);
         return orderId;
 
     }
 
-    @Override
-    public double getAmountFromParsingString(String convertedLine) throws NumberFormatException {
+
+    private double getAmountFromParsingString(String convertedLine) throws NumberFormatException {
         String amountStringFormat = convertedLine
                 .substring(convertedLine.indexOf("amount "), convertedLine.indexOf("currency"))
                 .substring(7)
                 .trim();
-            double amount = Double.parseDouble(amountStringFormat);
+        double amount = Double.parseDouble(amountStringFormat);
         return amount;
     }
 
-    @Override
-    public String getFileNameFromParsingString(String convertedLine) {
+
+    private String getFileNameFromParsingString(String convertedLine) {
         String fileName = convertedLine
                 .substring(convertedLine.indexOf("filename "), convertedLine.indexOf("line"))
                 .substring(9)
@@ -89,8 +107,8 @@ public class ConvertToOutputFormat implements Converter {
         return fileName;
     }
 
-    @Override
-    public int getLineNumberFromParsingString(String convertedLine) {
+
+    private int getLineNumberFromParsingString(String convertedLine) {
         String lineStringFormat = convertedLine
                 .substring(convertedLine.indexOf("line "))
                 .substring(5)
--- src/main/java/converter/Converter.java
+++ src/main/java/converter/Converter.java
@@ -1,16 +1,32 @@
 package converter;
 
 import output.OutputObject;
+import parser.CustomParser;
 
 import java.util.List;
 import java.util.Map;
 
- interface Converter {
+/**
+ * @author Semen Nikolaenko
+ * @version 1
+ */
+interface Converter {
+    /**
+     *
+     * @param linesReadyToConvert
+     * @return OutputObject readyObject
+     * метод возвращает готовый объект требуемого формата
+     * все параметры для создания объекта получаются из метода
+     * {@link Converter#getParamsForFutureObject(String)}
+     */
     public OutputObject getReadyObject(String linesReadyToConvert);
+
+    /**
+     *
+     * @param convertedLine это преобразованая строка которую можно получить в классе {@link CustomParser#getWorkResult()}
+     * @return Map<String,String> на выходе получается мапа ключи который являются именами параметров
+     * а значения соответственно значениями
+     */
     public Map<String, String> getParamsForFutureObject(String convertedLine);
-     public String getCommentFromParsingString(String convertedLine);
-     public int getOrderIdtFromParsingString(String convertedLine);
-     public double getAmountFromParsingString(String convertedLine);
-     public String getFileNameFromParsingString(String convertedLine);
-     public int getLineNumberFromParsingString(String convertedLine);
+
 }
